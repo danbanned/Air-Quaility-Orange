5f4585d07298307776ac87f95c197fda5d13ea44
danbanned <Djohn0082@launchpadphilly.org>
2026-04-04 19:16:53 -0700

    verecl

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,19 +1,26 @@
 // components/Layout/Layout.js
+'use client';
+
 import React from 'react';
+import { usePathname } from 'next/navigation';
 import Header from './Header';
 import Footer from './Footer';
 import SiteBackground from './SiteBackground';
 import './Layout.css';
 
 const Layout = ({ children, userId }) => {
+  const pathname = usePathname();
+  const immersiveRoutes = new Set(['/AQStories', '/simple-map']);
+  const isImmersiveRoute = immersiveRoutes.has(pathname);
+
   return (
     <div className="layout">
       <SiteBackground />
-      <Header userId={userId} />
-      <main className="main-content">
+      {!isImmersiveRoute && <Header userId={userId} />}
+      <main className={`main-content ${isImmersiveRoute ? 'main-content-immersive' : ''}`}>
         <div className="main-shell">{children}</div>
       </main>
-      <Footer />
+      {!isImmersiveRoute && <Footer />}
     </div>
   );
 };
