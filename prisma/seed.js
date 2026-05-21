807b251dcd07fa0753b43e93b3284f2359f704a8
Daniel <Djohn0082@launchpadphilly.org>
2026-05-21 15:01:44 -0400

    move sqlite db into deployable folder

--- prisma/seed.js
+++ prisma/seed.js
@@ -4,7 +4,7 @@ const { PrismaBetterSqlite3 } = require('@prisma/adapter-better-sqlite3');
 const { PrismaClient } = require('@prisma/client');
 
 const adapter = new PrismaBetterSqlite3({
-  url: process.env.DATABASE_URL || 'file:./dev.db',
+  url: process.env.DATABASE_URL || 'file:./prisma/dev.db',
 });
 
 const prisma = new PrismaClient({ adapter });
